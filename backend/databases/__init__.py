e0b5e9480ff4482d0932eb517ccc597b2679927e
MrFace55 <Serebryakov322@gmail.com>
2025-03-15 02:46:48 +0400

    refactored database_uri; added python dockerfile, db dockerfile, docker-compose. CONTAINERS ARE WORKING NOW YAAAAPIIII

--- backend/databases/__init__.py
+++ backend/databases/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:appasonya2@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
